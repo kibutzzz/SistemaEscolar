b75c1eea45c592bba600d5bf6ae49327339ace88
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2018-08-31 22:03:15 -0300

    adicionada a tela de inserção de materia

---
+++ app/src/main/java/com/example/a20151inf0107/sistemaescolar/InsertMateriaActivity.java
@@ -0,0 +1,65 @@
+package com.example.a20151inf0107.sistemaescolar;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.AppCompatSpinner;
+import android.util.Log;
+import android.view.View;
+import android.widget.SimpleCursorAdapter;
+
+public class InsertMateriaActivity extends AppCompatActivity {
+
+    private static final String TAG = "LOG TAG ---";
+    AppCompatEditText editTextMateria;
+    AppCompatSpinner spinnerProfessor;
+    AppCompatButton buttonCadastrar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_materia);
+
+
+        editTextMateria = findViewById(R.id.materia);
+        spinnerProfessor = findViewById(R.id.professor);
+        buttonCadastrar = findViewById(R.id.button_cadastrar);
+
+
+
+
+        buttonCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v(TAG, spinnerProfessor.getSelectedItemId() + "");
+            }
+        });
+    }
+
+    private void setSpinnerValues(){
+        BancoController crud = new BancoController(getBaseContext());
+        Cursor cursor = crud.carregaRegistrosProfessor();
+
+        String[] columns = {
+                CriaBanco.NOME,
+                CriaBanco.ID
+        };
+
+        int[] views = {
+                android.R.id.text1
+        };
+
+        SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_spinner_dropdown_item,
+                cursor,
+                columns,
+                views,
+                0);
+        cursorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerProfessor.setAdapter(cursorAdapter);
+    }
+}
